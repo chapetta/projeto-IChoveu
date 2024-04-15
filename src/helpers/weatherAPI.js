98940c10640d90ec2f1a0df385611a7a1c351fc1
chapetta <cha.petta@hotmail.com>
2024-04-15 19:45:06 -0300

    tentando corrigir para passar no test

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,5 @@
-const TOKEN = 'a7be3227909146b5b44203434241504';
-
 export const searchCities = async (term) => {
-  const API_URL = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`;
+  const API_URL = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${import.meta.env.VITE_TOKEN}&q=${term}`;
   const response = await fetch(API_URL);
   const data = await response.json();
   if (data.length > 0) {
@@ -11,7 +9,7 @@ export const searchCities = async (term) => {
 };
 
 export const getWeatherByCity = async (cityURL) => {
-  const API_URL = `http://api.weatherapi.com/v1/current.json?lang=pt&key=${TOKEN}&q=${cityURL}`;
+  const API_URL = `http://api.weatherapi.com/v1/current.json?lang=pt&key=${import.meta.env.VITE_TOKEN}&q=${cityURL}`;
   const response = await fetch(API_URL);
   const { location, current } = await response.json();
   return {
